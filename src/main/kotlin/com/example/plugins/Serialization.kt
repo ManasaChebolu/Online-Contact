8fac48afb5f06a64e61a126e3c3300a8f9b90ec1
ManasaChebolu <129951693+ManasaChebolu@users.noreply.github.com>
2023-09-21 01:02:18 -0700

    Serialization.kt

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -3,10 +3,14 @@ package com.example.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 
 fun Application.serialization() {
     install(ContentNegotiation) {
-           json()
+           json(Json {
+               prettyPrint=true
+               encodeDefaults=true
+           })
     }
 }
